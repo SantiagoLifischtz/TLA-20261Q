5a4f6c04acd48bf84ef36cc2f8635d392b1a98ab
Santiago Lifischtz <santilifischtz@gmail.com>
2026-05-07 11:47:03 -0300

    non-terminal typedefs

--- src/main/c/frontend/syntactic-analysis/AbstractSyntaxTree.h
+++ src/main/c/frontend/syntactic-analysis/AbstractSyntaxTree.h
@@ -20,7 +20,44 @@ typedef enum FactorType FactorType;
 typedef struct Constant Constant;
 typedef struct Expression Expression;
 typedef struct Factor Factor;
+
 typedef struct Program Program;
+typedef struct Definitions Definitions;
+typedef struct Definition Definition;
+typedef struct Play Play;
+typedef struct TrackIDs TrackIDs;
+typedef struct ConfigSentence ConfigSentence;
+typedef struct ConfigSentenceContent ConfigSentenceContent;
+typedef struct Tempo Tempo;
+typedef struct Key Key;
+typedef struct Mode Mode;
+typedef struct Pitch Pitch;
+typedef struct NoteLetter NoteLetter;
+typedef struct SemitoneModifier;
+typedef struct Track Track;
+typedef struct Instrument Instrument;
+typedef struct Sentences Sentences;
+typedef struct Sentence Sentence;
+typedef struct PatternDefinition PatternDefinition;
+typedef struct Pattern Pattern;
+typedef struct WaitAfterInline WaitAfterInline;
+typedef struct WaitAfterBlock WaitAfterBlock;
+typedef struct InlinePattern InlinePattern;
+typedef struct Note Note;
+typedef struct NoteID NoteID;
+typedef struct Duration Duration;
+typedef struct Rest Rest;
+typedef struct Repeat Repeat;
+typedef struct Step Step;
+typedef struct Block Block;
+typedef struct Chord Chord;
+typedef struct PatternChord PatternChord;
+typedef struct CommaSeparatedPatterns CommaSeparatedPatterns;
+typedef struct NoteChord NoteChord;
+typedef struct CommaSeparatedNotes CommaSeparatedNotes;
+typedef struct Strum Strum;
+typedef struct Arpeggio Arpeggio;
+typedef struct Degree Degree;
 
 /**
  * Node types for the Abstract Syntax Tree (AST).
@@ -74,5 +111,7 @@ void destroyConstant(Constant * constant);
 void destroyExpression(Expression * expression);
 void destroyFactor(Factor * factor);
 void destroyProgram(Program * program);
+
+// TODO add destroy functions
 
 #endif
