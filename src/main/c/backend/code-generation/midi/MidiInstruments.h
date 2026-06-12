b0b173691f3a3fb6f42fae60af6fbe77ca2c2dd0
SebaNLee <seba.nico.lee@gmail.com>
2026-06-12 17:37:16 -0300

    feat: midi instrument name to number mapper

--- src/main/c/backend/code-generation/midi/MidiInstruments.h
+++ src/main/c/backend/code-generation/midi/MidiInstruments.h
@@ -1,7 +1,11 @@
 #ifndef MIDI_INSTRUMENTS_HEADER
 #define MIDI_INSTRUMENTS_HEADER
 
+#include <stdint.h>
 
-// TODO
+/**
+ * @ref https://midimusic.github.io/tech/midispec.html#BMA1_4
+ */
+uint8_t getMidiInstrumentNumber(const char * instrumentName);
 
 #endif
