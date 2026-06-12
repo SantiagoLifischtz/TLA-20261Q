18eb433425a7d48fc131446eaf1a3005bd1f7ca2
SebaNLee <seba.nico.lee@gmail.com>
2026-06-12 17:03:55 -0300

    feat: generator creates midi header

--- src/main/c/backend/code-generation/Generator.c
+++ src/main/c/backend/code-generation/Generator.c
@@ -20,6 +20,26 @@ ModuleDestructor initializeGeneratorModule() {
 
 /** PRIVATE FUNCTIONS */
 
+static void _writeMThd(FILE * output, uint16_t numTracks);
+
+//
+//
+
+
+
+
+static void _writeMThd(FILE * output, uint16_t numTracks) {
+	BufferADT buffer = bufferNew();
+
+	bufferAppendData(buffer, MIDI_CHUNK_TYPE_HEADER, 4);
+	bufferAppendU32BE(buffer, MIDI_HEADER_CHUNK_LENGTH);
+	bufferAppendU16BE(buffer, MIDI_FORMAT_MULTI_TRACK);
+	bufferAppendU16BE(buffer, numTracks);
+	bufferAppendU16BE(buffer, MIDI_TICKS_PER_QUARTER);
+	fwrite(bufferGetData(buffer), 1, bufferGetLength(buffer), output);
+
+	bufferFree(buffer);
+}
 
 /** PUBLIC FUNCTIONS */
 
@@ -31,6 +51,9 @@ void executeGenerator(CompilerState * compilerState) {
 		logError(_logger, "DefinitionTables build error.");
 		return;
 	}
+	// TODO decisión de diseño: fwrite a archivo en particular o a STDOUT y que el usuario haga pipe?
+	_writeMThd(stdout, 1); // TODO, cantidad de tracks obtenerlo de DefinitionTables
+	// TODO generar contenido MIDI
 	destroyDefinitionContext(&context);
 	logDebugging(_logger, "Generation is done.");
 }
