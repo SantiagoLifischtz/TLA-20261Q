cccb628bdc36f33537b28badf13aba2730ffb552
SebaNLee <seba.nico.lee@gmail.com>
2026-06-14 02:34:55 -0300

    feat: new midi constants

--- src/main/c/backend/code-generation/midi/MidiConstants.h
+++ src/main/c/backend/code-generation/midi/MidiConstants.h
@@ -10,6 +10,8 @@
 #define MIDI_PERCUSSION_CHANNEL 9
 #define MIDI_DEFAULT_VELOCITY   0x40
 
+#define MIDI_TEMPO_FROM_BPM(bpm) ((uint32_t)(60000000.0f / (bpm)))
+
 #define DSL_DRUMS_INSTRUMENT_NAME "Drums"
 
 #endif
