84fd996bd5f2fee987634a40f0a7d0e6b29489d9
SebaNLee <seba.nico.lee@gmail.com>
2026-06-14 02:42:42 -0300

    feat: midi tempo event builder

--- src/main/c/backend/code-generation/midi/MidiEvents.h
+++ src/main/c/backend/code-generation/midi/MidiEvents.h
@@ -1,6 +1,8 @@
 #ifndef MIDI_EVENTS_HEADER
 #define MIDI_EVENTS_HEADER
 
+#include <stdint.h>
+
 #define MIDI_NOTE_OFF          0x80
 #define MIDI_NOTE_ON           0x90
 #define MIDI_PROGRAM_CHANGE    0xC0
@@ -8,5 +10,7 @@
 #define MIDI_META_EVENT        0xFF
 #define MIDI_META_SET_TEMPO    0x51
 #define MIDI_META_END_OF_TRACK 0x2F
+
+void buildTempoEvent(uint8_t out[6], uint32_t quarterUsecs);
 
 #endif
